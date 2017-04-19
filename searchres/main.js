aacfcb13743012d6d3bddf5c2a9b1c2bb5a78729
Ming Wang <mingwang@mit.edu>
2017-04-19 13:38:33 -0400

    heart working

--- searchres/main.js
+++ searchres/main.js
@@ -3,3 +3,17 @@ $(document).ready(function(){
 	$(".cbox").prop('checked', true);
 
 });
+
+//change
+$(document).on('click', '.fav', function(evt){
+	if ($(evt.target).attr('src') == '../images/hearticon-red.png') {
+		$(evt.target).attr('src', '../images/hearticon-pink.png');
+		//remove popup
+	} else {
+
+		$(evt.target).attr('src', '../images/hearticon-red.png');
+		//add popup
+
+	}
+
+});
