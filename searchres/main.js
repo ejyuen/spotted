5a32f0141d43e137a09a6280bc7d8c88a9388e52
Ming Wang <mingwang@mit.edu>
2017-04-19 19:27:51 -0400

    further changes

--- searchres/main.js
+++ searchres/main.js
@@ -2,9 +2,17 @@ $(document).ready(function(){
 	//set all checkboxes as checked
 	$(".cbox").prop('checked', true);
 
+	$('#exampleModal').modal({show: false});
+
+	$('[data-toggle="popover"]').popover();
+
 });
 
-//change
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+});
+
+//Adding and removing from favorites
 $(document).on('click', '.fav', function(evt){
 	if ($(evt.target).attr('src') == '../images/hearticon-red.png') {
 		$(evt.target).attr('src', '../images/hearticon-pink.png');
@@ -15,5 +23,13 @@ $(document).on('click', '.fav', function(evt){
 		//add popup
 
 	}
+});
 
+//pet preview
+$(document).on('click', '.petprev', function(evt) {
+	$('#gatsby').modal('show');
+
+});
+$(document).on('click', '.petinf', function(evt) {
+	$('#gatsby').modal('show');
 });
